5794c70a23bc7d3a3cacae8ea3bb9dcb35cbc9f4
503945930 <503945930@qq.com>
2018-07-23 19:46:51 +0800

    add block height

--- src/components/possessions/possessions-info.ts
+++ src/components/possessions/possessions-info.ts
@@ -1,27 +1,73 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
 import { NavController } from 'ionic-angular'
 import { Account } from '../../shared/typings'
+import { ApiProvider } from '../../providers'
+import { interval } from 'rxjs/observable/interval'
 
 @Component({
 	selector: 'possessions-info',
 	template: `
 		<ion-card class="card-info">
-				<div class="card-avatar" (click)="openQRCode()">
-					<img src="assets/imgs/icon-qrcode-avatar.png" alt="">
-				</div>
+			<div class="block-height">
+				<p class="height"> {{ blockHeight }} </p>
+				<p class="countdown"> {{ countdown }} </p>
+			</div>
+			<div class="card-avatar" (click)="openQRCode()">
+				<img src="assets/imgs/icon-qrcode-avatar.png" alt="">
+			</div>
 		  <div class="card-title">{{ account?.address | addressCollapse }}</div>
-			<div class="bg"></div>
+		 <div class="bg"></div>
 	  </ion-card>
-		<div class="card-round"></div>
+	  <div class="card-round"></div>
 	`
 })
-export class PossessionsInfoComponent {
+export class PossessionsInfoComponent implements OnInit {
 	@Input() account: Account
 
-	constructor (private navCtrl: NavController) {}
+	private  blockHeight = 0
+	private  countdown = 0
 
-	openQRCode () {
+	constructor(private navCtrl: NavController,
+		private apiProvider: ApiProvider,
+	) { }
+
+	openQRCode() {
 		this.navCtrl.push('payment-qrcode', { address: this.account && this.account.address })
 	}
+
+	async ngOnInit() {
+		// this.blockHeight = await this.getHeight()
+
+		this.getHeight()
+		this.getCountDown()
+	}
+
+	async getHeight() {
+
+		interval(3000).subscribe(async () => {
+			const result = await this.apiProvider.get('height').toPromise()
+			if (result && Number(result.height) > Number(this.blockHeight)) {
+
+				this.countdown = 0
+				this.blockHeight = result.height
+				// return result.height
+			}
+
+			// return 0
+		})
+
+		// console.log('result', result)
+	}
+
+	getCountDown() {
+		interval(1000).subscribe(() => {
+			this.countdown ++
+
+			console.log('getCountDown', this.countdown)
+		})
+
+
+	}
+
 }
 

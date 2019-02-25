5e5b6723dec58a6e084ca7d9b977cc1a359e096e
503945930 <503945930@qq.com>
2019-02-25 13:17:17 +0800

    add seac seas swap

--- src/components/possessions/possession-detail/possession-detail-bgcard.ts
+++ src/components/possessions/possession-detail/possession-detail-bgcard.ts
@@ -27,7 +27,7 @@ const { generateSignature, getPrivateKeyFromWIF } = wallet
 
 			<button  color="light"
 			*ngIf="balance.hash === 'a52e3e99b6c2dd2312a94c635c050b4c2bc2485fcb924eecb615852bd534a63f' || balance.hash === '30e9636bc249f288139651d60f67c110c3ca4c3dd30ddfa3cbcec7bb13f14fd4'"
-			 ion-button round full  class="po-btn" (click)="swap(balance.hash)" [disabled]="balance.amount <= 0">
+			 ion-button round full  class="po-btn" (click)="swap(balance.hash)" [disabled]="balance.amount <= 0 || !btnSwap">
 			 {{ 'POSSESSIONS.DETAILS.swap' | translate }}
 			</button>
 
@@ -49,6 +49,7 @@ export class PossessionDetailBgcard {
 	// @Input() scrollTop = 0
 	// @Input() differentScrollTop = 0
 	@Input() balance: IBalance
+	private btnSwap = true
 	// @Input() price: number
 
 	// downBoundary = window.outerHeight * 0.2
@@ -97,12 +98,16 @@ export class PossessionDetailBgcard {
 					const pr = getPrivateKeyFromWIF(data.wif)
 					const sign = this.generateSignatureAndData(result.transaction, pr)
 					console.log('swap:sign', sign)
-					const res = await this.apiProvider.broadcast(sign).toPromise()
+					const res: any = await this.apiProvider.broadcast(sign).toPromise()
 					console.log('res', res)
 
-					this.ts.get('POSSESSIONS.DETAILS.swap_success').subscribe(d => {
-						this.notificationProvider.emit({ message: d })
-					})
+					if (res.result) {
+						this.btnSwap = false
+						this.ts.get('POSSESSIONS.DETAILS.swap_success').subscribe(d => {
+							this.notificationProvider.emit({ message: d })
+						})
+					}
+
 				} catch (error) {
 					console.log('swap:error', error)
 
